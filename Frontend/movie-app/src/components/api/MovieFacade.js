33cc3e3636b26452bb415edc492ec0b94a173f86
Frederik Dahl <cph-fd76@cphbusiness.dk>
2022-05-23 14:36:24 +0200

    added movielist frontend
    Needs review information

---
+++ Frontend/movie-app/src/components/api/MovieFacade.js
@@ -0,0 +1,41 @@
+import { SERVER_URL } from "../../util/Settings";
+
+function handleHttpErrors(res) {
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, fullError: res.json() });
+  }
+  return res.json();
+}
+
+function movieFacade() {
+  const getMovies = () => {
+    const options = makeOptions("GET");
+
+    return fetch(SERVER_URL + "Movie/movie/", options)
+      .then(handleHttpErrors)
+      .then((res) => {
+        return res;
+      });
+  };
+
+  const makeOptions = (method, body) => {
+    var opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+  };
+
+  return {
+    makeOptions,
+    getMovies,
+  };
+}
+const facade = movieFacade();
+export default facade;
--- Frontend/movie-app/src/pages/Home.js
+++ Frontend/movie-app/src/pages/Home.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import facade from "../components/api/MovieFacade";
 
 export default function Home({ user }) {
-  return <div>{user.Username}</div>;
+  const [movieList, setMovieList] = useState(null);
+
+  useEffect(() => {
+    if (!movieList) {
+      const movies = facade.getMovies();
+      movies.then((e) => setMovieList(e));
+    }
+  }, [movieList]);
+
+  function createCard(title, tagline, released, rating) {
+    return (
+      <div class="moviecardcontainer">
+        <div class="moviecard">
+          <h4>
+            <b>{title}</b>
+          </h4>
+          <p>{tagline}</p>
+          <p>{released}</p>
+        </div>
+        <div class="movierating">
+          <p>Rating: {rating}</p>
+          <p> lorem ipsum dorsum porsum bla bal bla </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <div className="mx-10 text-center">
+          <h1>MOVIE MAFIA</h1>
+          <h3>
+            Welcome back <b>{user.Username}</b>
+          </h3>
+          <p>
+            Below is the list of all our <b>Movies</b> and <b>Series</b> and
+            their ratings.
+          </p>
+          <p>If you wish to add your own rating click the add rating button</p>
+        </div>
+      </div>
+      <div>
+        {movieList
+          ? movieList.map((e) => createCard(e.Title, e.Tagline, e.Released, 10))
+          : null}
+      </div>
+    </div>
+  );
 }
