bca8ed9dec39a171d42a4472426ebe647e7f451c
Sebastian <cph-sb287@cphbusiness.dk>
2022-05-24 13:27:54 +0200

    reviews cached

--- Backend/API/DatabaseExamAPI/DatabaseExamAPI/Controllers/ReviewController.cs
+++ Backend/API/DatabaseExamAPI/DatabaseExamAPI/Controllers/ReviewController.cs
@@ -12,11 +12,13 @@ namespace DatabaseExamAPI.Controllers
     {
         private readonly ILogger<ReviewController> _logger;
         private readonly ReviewFacade _reviewFacade;
+        private readonly CacheFacade _cacheFacade;
 
         public ReviewController(ILoggerFactory lf)
         {
             _logger = lf.CreateLogger<ReviewController>();
             _reviewFacade = new ReviewFacade(lf);
+            _cacheFacade = new CacheFacade();
         }      
 
         [HttpGet]
@@ -25,10 +27,21 @@ namespace DatabaseExamAPI.Controllers
         [ProducesResponseType(404)]
         public IActionResult GetReviewsByUserId(string userId)
         {
+
+            var cached = Task.Run(() => _cacheFacade.ReadData("review" + userId));
+            cached.Wait();
+            if (cached.Result != null)
+            {
+                return Ok(cached.Result);
+            }
+
             List<ReviewModel> reviews = _reviewFacade.GetReviewsByUserId(userId);
 
             if (reviews.Count > 0)
+            {
+                _cacheFacade.saveData(("review" + userId), reviews);
                 return Ok(reviews);
+            }
             return NotFound("No reviews found...");
 
         }
@@ -39,10 +52,20 @@ namespace DatabaseExamAPI.Controllers
         [ProducesResponseType(404)]
         public IActionResult GetReviewsByMovieId(string movieId)
         {
+            var cached = Task.Run(() => _cacheFacade.ReadData("rating" + movieId));
+            cached.Wait();
+            if (cached.Result != null)
+            {
+                return Ok(cached.Result);
+            }
+
             List<ReviewModel> reviews = _reviewFacade.GetReviewsByMovieId(movieId);
 
             if (reviews.Count > 0)
+            {
+                _cacheFacade.saveData(("rating"+movieId), reviews);
                 return Ok(reviews);
+            }
             return NotFound("No reviews found...");
 
         }
@@ -53,11 +76,20 @@ namespace DatabaseExamAPI.Controllers
         [ProducesResponseType(404)]
         public IActionResult GetAvgRatingByMovieId(string movieId)
         {
-            
+            var cached = Task.Run(() => _cacheFacade.ReadData("avgRating" + movieId));
+            cached.Wait();
+            if (cached.Result != null)
+            {
+                return Ok(cached.Result);
+            }
+
             double reviewRating = _reviewFacade.GetAvgRatingByMovieId(movieId);
 
             if (reviewRating != 0)
+            {
+                _cacheFacade.saveData(("avgRating" + movieId), reviewRating);
                 return Ok(reviewRating);
+            }
             return NotFound("No reviews found...");
         }
 
